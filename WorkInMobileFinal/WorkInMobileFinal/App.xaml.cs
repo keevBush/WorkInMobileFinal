2624f42515a2600b76a81433e7987a0126aeae3c
keevbush <keevbush@gmail.com>
2019-08-11 16:44:10 +0200

    project config

--- WorkInMobileFinal/WorkInMobileFinal/App.xaml.cs
+++ WorkInMobileFinal/WorkInMobileFinal/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,14 +12,23 @@ namespace WorkInMobileFinal
         {
             InitializeComponent();
             XF.Material.Forms.Material.Init(this);
-            MainPage = new NavigationPage (new Views.SplashScreen());
+            MainPage = new NavigationPage (new Views.PublicationPage());
         }
 
         protected override void OnStart()
         {
             // Handle when your app starts
+            if (StorageHelpers.SecureStorageHelper.AuthKey != null)
+                AppCenter.SetUserId(StorageHelpers.SecureStorageHelper.AuthKey);
+            AppCenter.Start("android=41b3b948-ee1e-452e-ae03-ea48653a1d60", typeof(Push));
+            Push.PushNotificationReceived += Push_PushNotificationReceived;
         }
-        
+
+        private void Push_PushNotificationReceived(object sender, PushNotificationReceivedEventArgs e)
+        {
+            var data = e;
+        }
+
         protected override void OnSleep()
         {
             // Handle when your app sleeps
