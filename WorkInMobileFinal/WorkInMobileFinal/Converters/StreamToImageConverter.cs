8985f5c9e1c82bfd351a3b2f8832a7457f44f8c8
keevbush <keevbush@gmail.com>
2019-08-11 16:39:29 +0200

    [add] converter byto to visual

---
+++ WorkInMobileFinal/WorkInMobileFinal/Converters/StreamToImageConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WorkInMobileFinal.Converters
+{
+    public class StreamToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ImageSource objImageSource;
+            //
+            if (value != null)
+            {
+               var bytImageData = (Stream)value;
+                //
+                objImageSource = ImageSource.FromStream(() => bytImageData);
+            }
+            else
+            {
+                objImageSource = null;
+            }
+            //
+            return objImageSource;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
