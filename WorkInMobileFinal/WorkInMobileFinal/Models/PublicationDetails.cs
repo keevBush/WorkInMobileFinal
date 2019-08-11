f313329d5ad8d6b6fe5726a6d4ff9f01909c9a31
keevbush <keevbush@gmail.com>
2019-08-11 16:40:17 +0200

    [add] model for detail publication

---
+++ WorkInMobileFinal/WorkInMobileFinal/Models/PublicationDetails.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkInMobileFinal.Models
+{
+    public class PublicationDetails
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("legende")]
+        public string Legende { get; set; }
+        [JsonProperty("value")]
+        public string Value { get; set; }
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+        [JsonProperty("tags")]
+        public IEnumerable<string> Tags { get; set; }
+        [JsonProperty("typePublication")]
+        public TypePublication TypePublication { get; set; }
+    }
+    public enum TypePublication
+    {
+        Image, Video, Text
+    }
+}
